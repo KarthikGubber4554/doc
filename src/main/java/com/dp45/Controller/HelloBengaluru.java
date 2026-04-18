f6f02e7371df65faf45a87902697f3494889709a
KarthikGubber4554 <your-email@example.com>
2026-04-18 14:10:52 +0530

    First Commit

---
+++ src/main/java/com/dp45/Controller/HelloBengaluru.java
@@ -0,0 +1,14 @@
+package com.dp45.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloBengaluru {
+	
+	@GetMapping("/message")
+	public String getMessage() {
+		return "HELLO BENGALURU";
+	}
+
+}
